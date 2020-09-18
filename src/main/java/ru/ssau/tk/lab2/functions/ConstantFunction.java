9b187f19abba7b8d26329683333796f73da173be
Timirbulatov <aidar126621@gmail.com>
2020-09-18 15:10:33 +0400

    добавлен класс ConstantFunction

---
+++ src/main/java/ru/ssau/tk/lab2/functions/ConstantFunction.java
@@ -0,0 +1,18 @@
+package ru.ssau.tk.lab2.functions;
+
+public class ConstantFunction implements MathFunction{
+    private final double constant;
+
+    public double getConstant() {
+        return constant;
+    }
+
+    public ConstantFunction (double constant){
+        this.constant=constant;
+    }
+
+    @Override
+    public double apply(double x) {
+        return 0;
+    }
+}
