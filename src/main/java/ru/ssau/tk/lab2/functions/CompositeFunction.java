bb273ff6ffa258f2acb513876215935e0caa663f
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-09-18 21:55:37 +0400

    Create CompositeFunction.java
    Nothing interesting, only fields

---
+++ src/main/java/ru/ssau/tk/lab2/functions/CompositeFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.lab2.functions;
+
+public class CompositeFunction implements MathFunction {
+    private MathFunction firstFunction;
+    private MathFunction secondFunctionG;
+
+}
