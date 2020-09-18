31d84319e10ff5494fa7e2bc7474f8caf7c48e5d
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-09-18 23:06:30 +0400

    Update CompositeFunction.java
    Finally finished CompositeFunc

--- src/main/java/ru/ssau/tk/lab2/functions/CompositeFunction.java
+++ src/main/java/ru/ssau/tk/lab2/functions/CompositeFunction.java
@@ -7,6 +7,9 @@ public class CompositeFunction implements MathFunction {
     public CompositeFunction (MathFunction firstFunction, MathFunction secondFunction){
         this.firstFunction = firstFunction;
         this.secondFunction = secondFunction;
-        
+    }
+
+    public double apply (double x){
+        return secondFunction.apply (firstFunction.apply (x));
     }
 }
