dbd044bd4432fc8bc711e72cd0c1164d13e4b4ec
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-09-18 21:39:27 +0400

    Create IdentityFunction.java
    Наконец-то сел прогать

---
+++ src/main/java/ru/ssau/tk/lab2/functions/IdentityFunction.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.lab2.functions;
+
+import ru.ssau.tk.lab2.functions.MathFunction;
+
+public class IdentityFunction implements MathFunction {
+    public double apply (double x){
+        return x;
+    }
+}
