45d0197c7ce8000cd4b61a89b33de3b10ce4a51d
Timirbulatov <aidar126621@gmail.com>
2020-09-18 10:57:28 +0400

    добавлены геттеры и сеттеры

--- src/main/java/ru/ssau/tk/lab2/functions/SqrFunction.java
+++ src/main/java/ru/ssau/tk/lab2/functions/SqrFunction.java
@@ -3,6 +3,14 @@ package ru.ssau.tk.lab2.functions;
 public class SqrFunction implements MathFunction {
     double number;
 
+    public double getNumber() {
+        return number;
+    }
+
+    public void setNumber(double number) {
+        this.number = number;
+    }
+
     @Override
     public double apply(double x) {
         return java.lang.Math.pow(number, 2);
