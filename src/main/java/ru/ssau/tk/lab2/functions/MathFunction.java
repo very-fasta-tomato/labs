4f2589b6b4f8aa20f3e6d969989010c8adf71f2e
Timirbulatov <aidar126621@gmail.com>
2020-09-19 08:05:07 +0400

    обновлен интерфейс MathFunction

--- src/main/java/ru/ssau/tk/lab2/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/lab2/functions/MathFunction.java
@@ -2,4 +2,8 @@ package ru.ssau.tk.lab2.functions;
 
 public interface MathFunction {
     double apply(double x);
+
+    default CompositeFunction andThen(MathFunction afterFunction){
+        return new CompositeFunction(this, afterFunction);
+    }
 }
