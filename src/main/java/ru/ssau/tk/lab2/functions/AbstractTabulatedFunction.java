11112c21f35eab8e6e54794d50f8032a57884f4b
Timirbulatov <aidar126621@gmail.com>
2020-10-02 18:10:42 +0400

    закончен класс AbstractTabulatedFunction

--- src/main/java/ru/ssau/tk/lab2/functions/AbstractTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/lab2/functions/AbstractTabulatedFunction.java
@@ -1,4 +1,32 @@
 package ru.ssau.tk.lab2.functions;
 
-public class AbstractTabulatedFunction {
+public abstract class AbstractTabulatedFunction {
+    protected int count;
+
+    protected abstract int floorIndexOfX(double x);
+
+    protected abstract double extrapolateLeft(double x);
+
+    protected abstract double extrapolateRight(double x);
+
+    protected abstract double interpolate(double x, int floorIndex);
+
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return (leftY + (x - leftX) * (rightY - leftY) / (rightX - leftX));
+    }
+
+    public double apply(double x) {
+        int index = indexOfX(x);
+
+        if (x < leftBound()) {
+            return (extrapolateLeft(x));
+        }
+        if (x > rightBound()) {
+            return (extrapolateRight(x));
+        }
+        if (index != -1) {
+            return (getY(index));
+        }
+        return (interpolate(x, floorIndexOfX(x)));
+    }
 }
