6e525a9b25bc440d76aac38e914be4eaad6df591
Timirbulatov <aidar126621@gmail.com>
2020-10-03 08:13:43 +0400

    начал писать тесты класса ArrayTabulatedFunction

---
+++ src/test/java/ru/ssau/tk/lab2/functions/ArrayTabulatedFunctionTest.java
@@ -0,0 +1,56 @@
+package ru.ssau.tk.lab2.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ArrayTabulatedFunctionTest {
+
+    @Test
+    public void testApply() {
+    }
+
+    @Test
+    public void testGetCount() {
+    }
+
+    @Test
+    public void testGetX() {
+    }
+
+    @Test
+    public void testGetY() {
+    }
+
+    @Test
+    public void testSetY() {
+    }
+
+    @Test
+    public void testLeftBound() {
+    }
+
+    @Test
+    public void testRightBound() {
+    }
+
+    @Test
+    public void testIndexOfX() {
+    }
+
+    @Test
+    public void testIndexOfY() {
+    }
+
+    @Test
+    public void testFloorIndexOfX() {
+    }
+
+    @Test
+    public void testExtrapolateLeft() {
+    }
+
+    @Test
+    public void testExtrapolateRight() {
+    }
+}
