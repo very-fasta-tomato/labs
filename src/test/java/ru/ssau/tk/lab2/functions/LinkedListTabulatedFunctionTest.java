f7230f1abc003010e119fc97478fd75f1d95dba7
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-10-10 11:58:01 +0400

    Added method to create objects for testing

--- src/test/java/ru/ssau/tk/lab2/functions/LinkedListTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/lab2/functions/LinkedListTabulatedFunctionTest.java
@@ -7,9 +7,9 @@ import static org.testng.Assert.*;
 public class LinkedListTabulatedFunctionTest {
     private final double[] valuesX = new double[]{-3., -2., -1., 0., 1., 2., 3.};
     private final double[] valuesY = new double[]{0.143, 2.185, -1.557, 0., 1.557, -2.185, -0.143};
-    private MathFunction tanFunc = new TanFunction();
-    private MathFunction powFunc = new PowFunction();
-    private MathFunction sqrFunc = new SqrFunction();
+    private final MathFunction tanFunc = new TanFunction();
+    private final MathFunction powFunc = new PowFunction();
+    private final MathFunction sqrFunc = new SqrFunction();
     private final LinkedListTabulatedFunction definedThroughList = new LinkedListTabulatedFunction(valuesX, valuesY);
     private final LinkedListTabulatedFunction definedThroughMathFunction = new LinkedListTabulatedFunction(tanFunc, 0, 20, 15);
 
@@ -17,12 +17,16 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testGetCount() {
+        TabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        TabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.getCount(), 7, delta);
         assertEquals(definedThroughMathFunction.getCount(), 15, delta);
     }
 
     @Test
     public void testGetX() {
+        TabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        TabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         for (int i = 0; i < 7; i++) {
             assertEquals(definedThroughList.getX(i), i - 3, delta);
         }
@@ -33,6 +37,8 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testGetY() {
+        TabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        TabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         for (int i = 0; i < 7; i++) {
             assertEquals(definedThroughList.getY(i), Math.tan(i - 3), delta);
         }
@@ -43,6 +49,8 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testSetY() {
+        TabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        TabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         definedThroughList.setY(5, 100500.);
         definedThroughMathFunction.setY(0, 1009.);
         assertEquals(definedThroughList.getY(5), 100500., delta);
@@ -51,18 +59,24 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testLeftBound() {
+        TabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        TabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.leftBound(), -3., delta);
         assertEquals(definedThroughMathFunction.leftBound(), 0., delta);
     }
 
     @Test
     public void testRightBound() {
+        TabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        TabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.rightBound(), 3., delta);
         assertEquals(definedThroughMathFunction.rightBound(), 20., delta);
     }
 
     @Test
     public void testIndexOfX() {
+        TabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        TabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.indexOfX(1.), 4);
         assertEquals(definedThroughList.indexOfX(-2.), 1);
         assertEquals(definedThroughList.indexOfX(1.1), -1);
@@ -73,6 +87,8 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testIndexOfY() {
+        TabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        TabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.indexOfY(2.185), 1);
         assertEquals(definedThroughList.indexOfY(5.0), -1);
         assertEquals(definedThroughList.indexOfY(-2.185), 5);
@@ -82,6 +98,8 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testFloorIndexOfX() {
+        LinkedListTabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        LinkedListTabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.floorIndexOfX(30.), 7);
         assertEquals(definedThroughList.floorIndexOfX(-1.7), 1);
         assertEquals(definedThroughList.floorIndexOfX(2.5), 5);
@@ -92,6 +110,8 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testExtrapolateLeft() {
+        LinkedListTabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        LinkedListTabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.extrapolateLeft(-4.0), -1.9655, delta);
         assertEquals(definedThroughList.extrapolateLeft(-5.0), -3.853, delta);
         assertEquals(definedThroughMathFunction.extrapolateLeft(-1.0), -4.924, delta);
@@ -101,6 +121,8 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testExtrapolateRight() {
+        LinkedListTabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        LinkedListTabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.extrapolateLeft(4.0), 14.439, delta);
         assertEquals(definedThroughList.extrapolateLeft(5.0), 16.486, delta);
         assertEquals(definedThroughMathFunction.extrapolateLeft(22.0), 107.542, delta);
@@ -110,11 +132,27 @@ public class LinkedListTabulatedFunctionTest {
 
     @Test
     public void testInterlateRight() {
+        LinkedListTabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        LinkedListTabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
         assertEquals(definedThroughList.interpolate(-1.5, 0), 3.2, delta);
         assertEquals(definedThroughList.interpolate(0.5, 3), 0.78, delta);
         assertEquals(definedThroughMathFunction.interpolate(14.9, 10), -3.81, delta);
         assertEquals(definedThroughMathFunction.interpolate(1.3, 0), 6.34, delta);
         assertEquals(definedThroughMathFunction.interpolate(10.8, 8), -3.19, delta);
+    }
+
+    @Test
+    public void testApply(){
+        LinkedListTabulatedFunction definedThroughList = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughList(valuesX, valuesY);
+        LinkedListTabulatedFunction definedThroughMathFunction = LinkedListTabulatedFunction.createTabulatedFunctionDefinedThroughMathFunction(tanFunc, 0, 20, 15);
+        assertEquals(definedThroughList.apply(-1.5), 0.31, delta);
+        assertEquals(definedThroughMathFunction.apply(14.9), -3.81, delta);
+        assertEquals(definedThroughList.apply(4.0), 1.89, delta);
+        assertEquals(definedThroughMathFunction.apply(22.0), 5.76, delta);
+        assertEquals(definedThroughList.apply(-4.0), -1.89, delta);
+        assertEquals(definedThroughMathFunction.apply(-5.0), -24.439, delta);
+
+
     }
 
     @Test
