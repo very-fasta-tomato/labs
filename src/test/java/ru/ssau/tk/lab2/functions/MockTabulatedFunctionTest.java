a277678b65ca6a86be1b145fb980adc65798716e
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-10-10 10:34:38 +0400

    Finished mock test

--- src/test/java/ru/ssau/tk/lab2/functions/MockTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/lab2/functions/MockTabulatedFunctionTest.java
@@ -6,9 +6,19 @@ import org.testng.annotations.Test;
 
 public class MockTabulatedFunctionTest {
     private final MockTabulatedFunction mockTabulatedFunctionTest = new MockTabulatedFunction();
+    private final double delta = 0.1;
 
     @Test
     public void abstractTabulatedFunctionTest(){
         assertEquals(mockTabulatedFunctionTest.getCount(), 2);
+        assertEquals(mockTabulatedFunctionTest.interpolate(0.5, 0), 0.5, delta);
+        assertEquals(mockTabulatedFunctionTest.interpolate(0.7, 0), 0.7, delta);
+        assertEquals(mockTabulatedFunctionTest.interpolate(0.9, 0), 0.9, delta);
+
+        assertEquals(mockTabulatedFunctionTest.apply(0), 0.0, delta);
+        assertEquals(mockTabulatedFunctionTest.apply(1), 1.0, delta);
+        assertEquals(mockTabulatedFunctionTest.apply(0.7), 0.7, delta);
+        assertEquals(mockTabulatedFunctionTest.apply(-0.5), -0.5, delta);
+        assertEquals(mockTabulatedFunctionTest.apply(1.5), 1.5, delta);
     }
 }
