dd7879ccfb0ec3900398bd3ffeac8b616fe9770d
Timirbulatov <aidar126621@gmail.com>
2020-10-17 13:17:27 +0400

    все еще исправления

--- src/main/java/ru/ssau/tk/lab2/exceptions/InterpolationException.java
+++ src/main/java/ru/ssau/tk/lab2/exceptions/InterpolationException.java
@@ -2,7 +2,7 @@ package ru.ssau.tk.lab2.exceptions;
 
 public class InterpolationException extends RuntimeException {
     public InterpolationException() {
-
+        super();
     }
 
     public InterpolationException(String message) {
