7eb36b763b0b85d5de4bad1e1622caf27b1f3590
Timirbulatov <aidar126621@gmail.com>
2020-10-17 09:07:08 +0400

    переписана реализация метода iterator в ArrayTabulatedFunction

--- src/main/java/ru/ssau/tk/lab2/functions/ArrayTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/lab2/functions/ArrayTabulatedFunction.java
@@ -2,6 +2,7 @@ package ru.ssau.tk.lab2.functions;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import ru.ssau.tk.lab2.exceptions.*;
 
@@ -133,6 +134,23 @@ public class ArrayTabulatedFunction extends AbstractTabulatedFunction {
 
     @Override
     public Iterator<Point> iterator() {
-        throw new UnsupportedOperationException();
+        return new Iterator<>() {
+            int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return i < count;
+            }
+
+            @Override
+            public Point next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Point point = new Point(xValues[i], yValues[i]);
+                i++;
+                return point;
+            }
+        };
     }
 }
