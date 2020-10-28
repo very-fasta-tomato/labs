ae9adc18c5c5c64090fd10f927f37e3bb7ab28cf
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-10-28 21:15:52 +0400

    Half done

--- src/main/java/ru/ssau/tk/lab2/operations/TabulatedDifferetialOperator.java
+++ src/main/java/ru/ssau/tk/lab2/operations/TabulatedDifferetialOperator.java
@@ -1,32 +1,29 @@
 package ru.ssau.tk.lab2.operations;
 
 import ru.ssau.tk.lab2.functions.*;
-import ru.ssau.tk.lab2.functions.factory.ArrayTabulatedFunctionFactory;
-import ru.ssau.tk.lab2.functions.factory.TabulatedFunctionFactory;
+import ru.ssau.tk.lab2.functions.factory.*;
 
-public class TabulatedDifferetialOperator implements DifferentialOperator{
+public class TabulatedDifferetialOperator implements DifferentialOperator<TabulatedFunction>{
     private TabulatedFunctionFactory factory;
-    private TabulatedFunction function;
 
-    DifferentialOperator<TabulatedFunction> parametr = new DifferentialOperator<TabulatedFunction>() {
-        @Override
-        public TabulatedFunction derive(TabulatedFunction function) {
-            return function;
-        }
 
-        @Override
-        public double apply(double x) {
-            return 0;
-        }
-    };
+    @Override
+    public TabulatedFunction derive (TabulatedFunction function){
+        return function;
+    }
 
-    public TabulatedDifferetialOperator(TabulatedFunctionFactory factory, TabulatedFunction function){
+    @Override
+    public double apply(double x){
+        return 0;
+    }
+
+    public TabulatedDifferetialOperator(TabulatedFunctionFactory factory){
         this.factory = factory;
-        this.function = function;
     }
 
     public TabulatedDifferetialOperator(){
         factory = new ArrayTabulatedFunctionFactory();
-        function = new ArrayTabulatedFunction();
     }
+
+
 }
