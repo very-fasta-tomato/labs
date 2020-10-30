76b8ae120eed7234a74ee2f865e79a07110464f6
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-10-31 01:45:43 +0400

    I have understood this f***ing streams!

--- src/main/java/ru/ssau/tk/lab2/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/lab2/io/FunctionsIO.java
@@ -3,11 +3,22 @@ package ru.ssau.tk.lab2.io;
 import ru.ssau.tk.lab2.functions.*;
 
 import ru.ssau.tk.lab2.functions.factory.*;
+import ru.ssau.tk.lab2.operations.TabulatedFunctionOperationService;
 
 import java.io.*;
 
 final class FunctionsIO {
-    static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory){
-        //DataInputStream in = new DataInputStream(InputStream());
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(function.getCount());
+        for (Point currentPoint : TabulatedFunctionOperationService.asPoints(function)){
+            out.writeDouble(currentPoint.x);
+            out.writeDouble(currentPoint.y);
+        }
+        out.flush();
     }
+    /*
+    static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory){
+        DataInputStream in = new DataInputStream(inputStream);
+    }*/
 }
