05c34775b206bd899543033b681fc51886e3c2c5
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-10-31 01:03:54 +0400

    Something misunderstandable in IO

---
+++ src/main/java/ru/ssau/tk/lab2/io/FunctionsIO.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.lab2.io;
+
+import ru.ssau.tk.lab2.functions.*;
+
+import ru.ssau.tk.lab2.functions.factory.*;
+
+import java.io.*;
+
+final class FunctionsIO {
+    static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory){
+        //DataInputStream in = new DataInputStream(InputStream());
+    }
+}
