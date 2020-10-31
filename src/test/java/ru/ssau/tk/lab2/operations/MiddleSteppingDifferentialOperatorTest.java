f4ea863127e3c6777762b6508ae543890ba2ce46
Timirbulatov <aidar126621@gmail.com>
2020-10-31 14:06:40 +0400

    исправлены тесты MiddleSteppingDifferentialOperatorTest и RightSteppingDifferentialOperatorTest

--- src/test/java/ru/ssau/tk/lab2/operations/MiddleSteppingDifferentialOperatorTest.java
+++ src/test/java/ru/ssau/tk/lab2/operations/MiddleSteppingDifferentialOperatorTest.java
@@ -10,8 +10,8 @@ public class MiddleSteppingDifferentialOperatorTest {
 
     @Test
     public void testDerive() {
-        final double step = 0.001;
-        SteppingDifferentialOperator differentialOperator = new RightSteppingDifferentialOperator(step);
+        final double step = 0.01;
+        SteppingDifferentialOperator differentialOperator = new MiddleSteppingDifferentialOperator(step);
         assertEquals(differentialOperator.derive(new SqrFunction()).apply(1), 2, 0.1);
         assertEquals(differentialOperator.derive(new SqrFunction()).apply(2), 4, 0.1);
     }
--- src/test/java/ru/ssau/tk/lab2/operations/RightSteppingDifferentialOperatorTest.java
+++ src/test/java/ru/ssau/tk/lab2/operations/RightSteppingDifferentialOperatorTest.java
@@ -10,9 +10,9 @@ public class RightSteppingDifferentialOperatorTest {
 
     @Test
     public void testDerive() {
-        final double step = 0.001;
+        final double step = 0.01;
         SteppingDifferentialOperator differentialOperator = new RightSteppingDifferentialOperator(step);
-        assertEquals(differentialOperator.derive(new SqrFunction()).apply(1), 2, 0.01);
-        assertEquals(differentialOperator.derive(new SqrFunction()).apply(2), 4, 0.01);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(1), 2.01, 0.01);
+        assertEquals(differentialOperator.derive(new SqrFunction()).apply(2), 4.01, 0.01);
     }
 }
