081873aff7b10e6bd91a9ab7a31fd7ddc1a9a1a1
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-11-10 08:48:51 +0400

    First task of lab writing information of TF to console

---
+++ src/main/java/ru/ssau/tk/lab2/concurrent/ReadWriteTask.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.lab2.concurrent;
+
+import ru.ssau.tk.lab2.functions.TabulatedFunction;
+
+public class ReadWriteTask implements Runnable{
+    private TabulatedFunction tabulatedFunction;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < tabulatedFunction.getCount(); i++){
+            double currentX = tabulatedFunction.getX(i);
+            double currentY = tabulatedFunction.getY(i);
+            System.out.println(Thread.currentThread().getName() + " before write: i = " + i + " x = " + currentX + " y = " + currentY);
+            tabulatedFunction.setY(i, currentY + 1);
+            currentY = tabulatedFunction.getY(i);
+            System.out.println(Thread.currentThread().getName() + " after write: i = " + i + " x = " + currentX + " y = " + currentY);
+        }
+    }
+}
