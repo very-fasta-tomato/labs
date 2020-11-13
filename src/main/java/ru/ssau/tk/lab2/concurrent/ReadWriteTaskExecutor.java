f362f75537413c76d6715838af604a59868120bd
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-11-14 00:14:05 +0400

    Trying in synchronization

--- src/main/java/ru/ssau/tk/lab2/concurrent/ReadWriteTaskExecutor.java
+++ src/main/java/ru/ssau/tk/lab2/concurrent/ReadWriteTaskExecutor.java
@@ -9,18 +9,22 @@ public class ReadWriteTaskExecutor {
     public static void main(String[] args){
         TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
         List<Thread> threadList = new ArrayList<>();
-        for (int i = 1; i <= 20; i++){
+        for (int i = 0; i < 20; i++){
             Thread newThread = new Thread(new ReadWriteTask(tabulatedFunction));
             threadList.add(newThread);
         }
         for (Thread currentTread: threadList){
             currentTread.start();
+            synchronized (currentTread){
+                
+            }
         }
         try{
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         }
         catch (InterruptedException e){
             e.printStackTrace();
         }
+        System.out.println(tabulatedFunction);
     }
 }
