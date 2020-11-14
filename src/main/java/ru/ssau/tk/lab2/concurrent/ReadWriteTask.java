a8776fe9c2c4ad6e11244d439bfcad93a3df1d88
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-11-14 12:10:20 +0400

    Synchronization done

--- src/main/java/ru/ssau/tk/lab2/concurrent/ReadWriteTask.java
+++ src/main/java/ru/ssau/tk/lab2/concurrent/ReadWriteTask.java
@@ -2,19 +2,20 @@ package ru.ssau.tk.lab2.concurrent;
 
 import ru.ssau.tk.lab2.functions.TabulatedFunction;
 
-public class ReadWriteTask implements Runnable{
+public class ReadWriteTask implements Runnable {
     private final TabulatedFunction tabulatedFunction;
 
-    public ReadWriteTask (TabulatedFunction tabulatedFunction){
+    public ReadWriteTask(TabulatedFunction tabulatedFunction) {
         this.tabulatedFunction = tabulatedFunction;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < tabulatedFunction.getCount(); i++){
+        for (int i = 0; i < tabulatedFunction.getCount(); i++) {
             double currentX = tabulatedFunction.getX(i);
-            double currentY = tabulatedFunction.getY(i);
-            synchronized (Thread.currentThread()){
+            double currentY;
+            synchronized (tabulatedFunction) {
+                currentY = tabulatedFunction.getY(i);
                 System.out.println(Thread.currentThread().getName() + " before write: i = " + i + " x = " + currentX + " y = " + currentY);
                 tabulatedFunction.setY(i, currentY + 1);
                 currentY = tabulatedFunction.getY(i);
--- src/main/java/ru/ssau/tk/lab2/concurrent/ReadWriteTaskExecutor.java
+++ src/main/java/ru/ssau/tk/lab2/concurrent/ReadWriteTaskExecutor.java
@@ -6,20 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadWriteTaskExecutor {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
         List<Thread> threadList = new ArrayList<>();
-        for (int i = 0; i < 20; i++){
+        for (int i = 0; i < 20; i++) {
             Thread newThread = new Thread(new ReadWriteTask(tabulatedFunction));
             threadList.add(newThread);
         }
-        for (Thread currentTread: threadList){
+        for (Thread currentTread : threadList) {
             currentTread.start();
         }
-        try{
+        try {
             Thread.sleep(1000);
-        }
-        catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println(tabulatedFunction);
