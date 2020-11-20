55e1037cce93f5c17e41beef6c0d6ae699929d0b
Timirbulatov <aidar126621@gmail.com>
2020-11-20 19:34:29 +0400

    закончен класс AddingMultiplyingTaskExecutor, добавлены блоки синхронизвции

--- src/main/java/ru/ssau/tk/lab2/concurrent/AddingTask.java
+++ src/main/java/ru/ssau/tk/lab2/concurrent/AddingTask.java
@@ -4,18 +4,27 @@ import ru.ssau.tk.lab2.functions.TabulatedFunction;
 
 public class AddingTask implements Runnable {
     private final TabulatedFunction tabulatedFunction;
+    private Runnable postRunAction;
 
     public AddingTask(TabulatedFunction tabulatedFunction) {
         this.tabulatedFunction = tabulatedFunction;
     }
 
+    public AddingTask(TabulatedFunction func, Runnable postRunAction) {
+        this.tabulatedFunction = func;
+        this.postRunAction = postRunAction;
+    }
+
     public void run() {
+        double currentX;
+        double currentY;
         for (int i = 0; i < tabulatedFunction.getCount(); i++) {
-            double currentX = tabulatedFunction.getX(i);
-            double currentY = tabulatedFunction.getY(i);
-            System.out.println(Thread.currentThread().getName() + "i =" + i + " x = " + currentX + " old y = " + currentY);
-            tabulatedFunction.setY(i, currentY + 3);
-            currentY = tabulatedFunction.getY(i);
+            currentX = tabulatedFunction.getX(i);
+            synchronized (tabulatedFunction) {
+                currentY = tabulatedFunction.getY(i);
+                System.out.println(Thread.currentThread().getName() + "i =" + i + " x = " + currentX + " old y = " + currentY);
+                tabulatedFunction.setY(i, currentY + 3);
+            }
             System.out.println(Thread.currentThread().getName() + "i =" + i + " x = " + currentX + " new y = " + currentY);
         }
     }
--- src/main/java/ru/ssau/tk/lab2/concurrent/MultiplyingTask.java
+++ src/main/java/ru/ssau/tk/lab2/concurrent/MultiplyingTask.java
@@ -4,19 +4,29 @@ import ru.ssau.tk.lab2.functions.TabulatedFunction;
 
 public class MultiplyingTask implements Runnable {
     private final TabulatedFunction tabulatedFunction;
+    private Runnable postRunAction;
 
     public MultiplyingTask(TabulatedFunction tabulatedFunction) {
         this.tabulatedFunction = tabulatedFunction;
     }
 
+    public MultiplyingTask(TabulatedFunction func, Runnable postRunAction) {
+        this.tabulatedFunction = func;
+        this.postRunAction = postRunAction;
+    }
+
     public void run() {
+        double currentX;
+        double currentY;
         for (int i = 0; i < tabulatedFunction.getCount(); i++) {
-            double currentX = tabulatedFunction.getX(i);
-            double currentY = tabulatedFunction.getY(i);
-            System.out.println(Thread.currentThread().getName() + "i =" + i + " x = " + currentX + " old y = " + currentY);
-            tabulatedFunction.setY(i, currentY * 10);
-            currentY = tabulatedFunction.getY(i);
+            currentX = tabulatedFunction.getX(i);
+            synchronized (tabulatedFunction) {
+                currentY = tabulatedFunction.getY(i);
+                System.out.println(Thread.currentThread().getName() + "i =" + i + " x = " + currentX + " old y = " + currentY);
+                tabulatedFunction.setY(i, currentY * 10);
+            }
             System.out.println(Thread.currentThread().getName() + "i =" + i + " x = " + currentX + " new y = " + currentY);
         }
     }
 }
+
